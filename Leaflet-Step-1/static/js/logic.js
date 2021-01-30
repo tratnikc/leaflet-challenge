cc289ef42b27882d2cfe871792fa9956ad287f10
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-01-29 21:28:09 -0500

    base maps

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,16 +1,55 @@
-// Creating map object
-var myMap = L.map("map", {
-  center: [34.0522, -118.2437],
-  zoom: 8
-});
-
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// create map layers
+// streetmap
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: API_KEY
-}).addTo(myMap);
+});
 
+// darkmap
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+});
+
+// darkmap
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/outdoors-v11",
+  accessToken: API_KEY
+});
+
+// define baseMaps object to hold base layers
+var baseMaps = {
+  "Street Map": streetmap,
+  "Dark Map": darkmap,
+  "Outdoors": outdoors
+};
+
+// Creating map object, attach streetmap, 
+var myMap = L.map("map", {
+  center: [34.0522, -118.2437],
+  zoom: 5,
+  layers: [streetmap]
+});
+
+
+
+// geojson data is loaded in the config file
+var geojson;
+
+// grab data with d3
+d3.json(allWeek, function (data) {
+  console.log(data);
+
+})
